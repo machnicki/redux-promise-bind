3088aebccfc274d34e618f87ccc3faaa4c6d0d8c
Miron Machnicki <miron@machnicki.com.pl>
2017-06-06 17:38:29 +0100

    small refactor

--- src/index.js
+++ src/index.js
@@ -18,57 +18,108 @@ const runPromisesInQueue = (groupName: string) => {
   }
 }
 
-const addPromiseToQueue = (groupName, callback) => {
+type Action = {
+  type: string,
+  metadata: string,
+  promise: Function,
+  promiseArg: Array<any>,
+  logic: string,
+  group: string,
+}
+
+type ExtendedAction = {
+  initAction: Action,
+  startAction: Object,
+  successAction: Function,
+  errorAction: Function,
+}
+
+// TODO prepare onCancel handler
+
+const createStartAction = ({ type, metadata }) => (
+  extendBy({
+    type: `${type}_START`,
+  }, { metadata })
+)
+
+const createSuccessAction = ({ type, metadata }) => data => (
+  extendBy({
+    type: `${type}_SUCCESS`,
+    payload: data,
+  }, { metadata })
+)
+
+const createErrorAction = ({ type, metadata }) => ex => (
+  extendBy({
+    type: `${type}_ERROR`,
+    payload: ex,
+  }, { metadata })
+)
+
+const takeLast = () => {
+
+}
+
+const dispatchPromise = (extendedAction: ExtendedAction, dispatch: Function) => {
+  const {
+    startAction,
+    successAction,
+    errorAction,
+    initAction: {
+      promise,
+      promiseArg,
+    },
+  } = extendedAction
+
+  dispatch(startAction)
+  return promise(...(Array.isArray(promiseArg) ? promiseArg : [promiseArg]))
+    .then((data) => {
+      dispatch(successAction(data))
+      return data
+    }, (ex) => {
+      dispatch(errorAction(ex))
+      // TODO change to throw an error
+      return ex
+    })
+}
+
+const addPromiseToQueue = (extendedAction: ExtendedAction, dispatch: Function) => {
+  const { initAction: { group } } = extendedAction
   let promiseInQueue
   const promise = new Promise((success, reject) => {
-    promiseInQueue = { callback, success, reject }
+    promiseInQueue = { callback: () => dispatchPromise(extendedAction, dispatch), success, reject }
   })
 
-  if (!(promisesInQueue[groupName] && promisesInQueue[groupName].length)) {
-    promisesInQueue[groupName] = [promiseInQueue]
-    runPromisesInQueue(groupName)
+  if (!(promisesInQueue[group] && promisesInQueue[group].length)) {
+    promisesInQueue[group] = [promiseInQueue]
+    runPromisesInQueue(group)
   } else {
-    promisesInQueue[groupName].push(promiseInQueue)
+    promisesInQueue[group].push(promiseInQueue)
   }
 
   return promise
 }
 
+const createExtendedAction = (action: Action): ExtendedAction => ({
+  initAction: action,
+  startAction: createStartAction(action),
+  successAction: createSuccessAction(action),
+  errorAction: createErrorAction(action),
+})
+
 const promiseBindMiddleware = (
   { dispatch } : { dispatch: Function },
-) => (next: Function) => (action: Object) => {
+) => (next: Function) => (action: Action) => {
   if (action && action.promise && typeof action.promise === 'function') {
-    const { type, metadata, promise, promiseArg, logic, group } = action
+    const extendedAction = createExtendedAction(action)
 
-    const dispatchPromise = () => {
-      dispatch(extendBy({
-        type: `${type}_START`,
-      }, { metadata }))
-
-      return promise(...(Array.isArray(promiseArg) ? promiseArg : [promiseArg]))
-        .then((data) => {
-          dispatch(extendBy({
-            type: `${type}_SUCCESS`,
-            payload: data,
-          }, { metadata }))
-
-          return data
-        }, (ex) => {
-          dispatch(extendBy({
-            type: `${type}_ERROR`,
-            payload: ex,
-          }, { metadata }))
-
-          // TODO change to throw an error
-          return ex
-        })
-    }
-
-    switch (logic) {
+    switch (action.logic) {
       case 'inQueue':
-        return addPromiseToQueue(group, dispatchPromise)
+        return addPromiseToQueue(extendedAction, dispatch)
+      case 'takeLast':
+        return takeLast(extendedAction, dispatch)
       default:
-        return dispatchPromise()
+        return dispatchPromise(extendedAction, dispatch)
     }
   }
   return next(action)
